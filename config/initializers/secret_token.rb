06328bcb788148705120043d268ec52bb8d185f7
Chris Masters <chrismasters@gmail.com>
2011-08-31 23:58:31 +0200

    Clean install of Rails 3.1 using postgresql for database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chrismasters::Application.config.secret_token = '3efc6523133bf677d9cd363bcb336cf0981d5d46f01eaeff1017d201c5b787c7f709fcd6d0e5409d33a7aa6f4cc2fecbdc2c075e296180666c63626fbdb02ef1'
